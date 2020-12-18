8e324bdee187adc9d54edd17e7f4a01cca52d7da
longbui <buingohoanglong2000@gmail.com>
2020-12-18 22:26:53 +0700

    update profile

--- application/views/Profile/index.php
+++ application/views/Profile/index.php
@@ -27,7 +27,7 @@
                             </tr>
                         </tbody>
                     </table>
-                    <button type="button" class="align-self-center btn btn-secondary">UPDATE PROFILE</button>
+                    <button type="button" class="align-self-center btn btn-secondary" data-toggle="modal" data-target="#staticBackdrop">UPDATE PROFILE</button>
                     <a href="./ChangePassword" class="align-self-center" style="font-size: 14px;font-weight: 600;" >Change password</a>
                 </div>
             </div>
@@ -40,5 +40,46 @@
             <img src="./public/images/product/astronaut.png" alt="">
         </div>
     </div>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="staticBackdropLabel">Update Profile</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">First Name:</label>
+                            <input type="text" class="form-control" id="firstname">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Last Name:</label>
+                            <input type="text" class="form-control" id="lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Phone:</label>
+                            <input type="number" class="form-control" id="phone">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 <?php $this->view("layout/footer"); ?>
