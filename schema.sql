27c35e493a201dd0e894cd03ba9774045e2d90b1
longbui <buingohoanglong2000@gmail.com>
2020-12-19 23:22:53 +0700

    add Course, Learn table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,24 @@ create table User (
     Password varchar(255),
     primary key (Email)
 );
+
+create table Course (
+    Code varchar(255),
+    Name varchar(255),
+    Price int unsigned,
+    StartDate date,
+    EndDate date,
+    LearningDay enum('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'),
+    StartHour time,
+    EndHour time,
+    Description varchar(1000),
+    primary key (Code)
+);
+
+create table Learn (
+    Email varchar(255),
+    Code varchar(255),
+    foreign key (Email) references User(Email),
+    foreign key (Code) references Course(Code),
+    primary key (Email, Code)
+);
