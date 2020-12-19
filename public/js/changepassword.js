80a27aa217f03714d0325ab4a754aff423e7d2b5
longbui <buingohoanglong2000@gmail.com>
2020-12-19 22:35:07 +0700

    changepassword

--- public/js/changepassword.js
+++ public/js/changepassword.js
@@ -1,29 +1,44 @@
 $(document).ready(function(){
     $("#btnSubmit").click(function() {
+        // get form input
         oldpassword = $("#oldpassword").val();
         newpassword = $("#newpassword").val();
         confirmpassword = $("#confirmpassword").val();
+        // reset form
         $("#oldpassword").val("");
         $("#newpassword").val("");
         $("#confirmpassword").val("");
-        if (oldpassword != '' && newpassword != '' && confirmpassword != '' && newpassword == confirmpassword) {
-            $.ajax({
-                url: 'http://localhost/Web_Assignment_02/ChangePassword/process',
-                type: 'post',
-                dataType: 'json',
-                data: {'oldpassword': oldpassword, 'newpassword': newpassword},
-                success: function (response) {
-                    if(response["status"] == "success") {
-                        $("#resultModalLabel").html("Success");
-                        $("#resultModalBody").html("Password reset successfully !");
-                    } else {
-                        $("#resultModalLabel").html("Fail");
-                        $("#resultModalBody").html("Cannot reset password !");
-                    }
-                    $('#resultModal').modal('show');
-                },
-            });
+        // validate input
+        if (oldpassword == '' || newpassword == '' || confirmpassword == '') {
+            $("#resultModalLabel").html("Error");
+            $("#resultModalBody").html("Empty field !");
+            $('#resultModal').modal('show');
+            return;
         }
+        if (newpassword != confirmpassword) {
+            $("#resultModalLabel").html("Error");
+            $("#resultModalBody").html("Password not match !");
+            $('#resultModal').modal('show');
+            return;       
+        }
+        // post request
+        $.ajax({
+            url: 'http://localhost/Web_Assignment_02/ChangePassword/process',
+            type: 'post',
+            dataType: 'json',
+            data: {'oldpassword': oldpassword, 'newpassword': newpassword},
+            success: function (response) {
+                if(response["status"] == "success") {
+                    $("#resultModalLabel").html("Success");
+                    $("#resultModalBody").html("Password reset successfully !");
+                } else {
+                    $("#resultModalLabel").html("Fail");
+                    $("#resultModalBody").html("Cannot reset password !");
+                }
+                $('#resultModal').modal('show');
+            },
+        });
+     
     });
 
     $("#btnOK").click(function() {
