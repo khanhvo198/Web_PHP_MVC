60f0777af5f55e2d4132188808d8f8535fe05881
longbui <buingohoanglong2000@gmail.com>
2020-12-21 01:34:46 +0700

    MyClass layout

---
+++ application/views/MyClass/coursecard.php
@@ -0,0 +1,9 @@
+<div class="col-lg-4 d-flex flex-column align-items-center">
+    <div class="card" style="width: 18rem;">
+        <img src="<?php echo $this->get_url("../public/images/myclass/courseimage.png") ?>" class="card-img-top" alt="...">
+        <div class="card-body p-1 d-flex flex-column align-items-center">
+            <h5 class="card-title">Programming Development</h5>
+            <a href="#" class="btn btn-secondary d-flex justify-content-center align-items-center">Launch</a>
+        </div>
+    </div>
+</div>
--- application/views/MyClass/index.php
+++ application/views/MyClass/index.php
@@ -8,7 +8,27 @@
 
 <?php $this->view("layout/header"); ?>
 
-    <div>My Class</div>
+    <div class="text my-5">On progress</div>
+    <div class="row mb-5">
+        <?php
+            for ($i = 0; $i < 5; $i++) {
+                require "./application/views/MyClass/coursecard.php";
+            } 
+        ?>
+    </div>
+
+
+    <div class="text my-5">Finished</div>
+    <div class="row mb-5">
+    <?php
+            for ($i = 0; $i < 2; $i++) {
+                require "./application/views/MyClass/coursecard.php";
+            } 
+        ?>
+    </div>
+
+        
+    <div class="text my-5"><a href="<?php echo $this->get_url("../Course") ?>">New Courses</a></div>
 
 
 <?php $this->view("layout/footer"); ?>
