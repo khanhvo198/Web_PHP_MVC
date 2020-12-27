f669f21acf0badf3fdde4bed5032a8aaaa2e9f7c
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2020-12-27 23:21:06 +0700

    Fix title

--- application/views/Admin/index.php
+++ application/views/Admin/index.php
@@ -59,7 +59,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h3 class="modal-title" id="edit-user-label">Edit User</h3>
+                        <h3 class="modal-title" id="edit-user-label">View User</h3>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
